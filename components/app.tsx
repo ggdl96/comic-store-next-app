15608d8986bddaf017e5d5795d81d6abc179606f
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2022-11-15 19:47:02 -0300

    update logic to match with one comic count

--- components/app.tsx
+++ components/app.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function AppComponent( { count } ) {
-  return <div><input placeholder='Buscar' /><span>{count}</span></div>
+  const text = `comic${count !== 1 ? 's': ''} ${count !== 1 ? 'are' : 'is'} available`;
+  return <div><input placeholder='Search' /><span>{count} {text}</span></div>
+}
+
+AppComponent.propTypes = {
+  count: PropTypes.number.isRequired,
 }
