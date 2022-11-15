8ba96638e509d57ec2fd8b9e54cd76e4787f3227
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2022-11-15 12:09:25 -0300

    add test to check redux logic

---
+++ components/SmartAppComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+
+import AppComponent from './app';
+import { ComicStore } from '../model/ComicStore/ComicStore';
+
+export default function AppComponentWithLogic() {
+    const comicsList = useSelector(state => state.comics.list)
+    const comicStore = new ComicStore(comicsList)
+
+  return <AppComponent count={comicStore.count()} />
+}
---
+++ components/appWithLogic.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { Provider, useSelector, useDispatch } from 'react-redux'
+
+import SmartAppComponent from './SmartAppComponent';
+import { store } from '../src/store';
+import { addList } from '../src/features/slice';
+
+describe('AppComponent', () => {
+  const InitialComponent = () => {
+    const dispatch = useDispatch();
+    dispatch(addList(['comic 1']));
+
+    return <SmartAppComponent />
+  }
+  it('given a provider with a comic on the list, counter should be 1', () => {
+
+    render(<Provider store={store}><InitialComponent /></Provider>);
+
+    expect(document.querySelector('span')).toHaveTextContent('1');
+  });
+});
