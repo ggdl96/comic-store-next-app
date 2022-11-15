2a782d938362b9b0d65bb6428040e9b359fa03b2
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2022-11-15 19:46:36 -0300

    add test for checking one comic count

--- components/app.spec.tsx
+++ components/app.spec.tsx
@@ -4,16 +4,22 @@ import '@testing-library/jest-dom'
 
 describe('AppComponent', () => {
   it('should have an input with "Buscar" placeholder', () => {
-    render(<AppComponent count="0" />);
+    render(<AppComponent count={0} />);
 
 
     expect(document.querySelector('input').getAttribute('placeholder')).toBe('Search');
   });
 
   it('should display total amount of comics', () => {
-    render(<AppComponent count="0" />);
+    render(<AppComponent count={0} />);
 
 
     expect(document.querySelector('span')).toHaveTextContent('0 comics are available');
+  });
+
+  it('should display total amount of comics', () => {
+    render(<AppComponent count={1} />);
+
+    expect(document.querySelector('span')).toHaveTextContent('1 comic is available');
   });
 });
