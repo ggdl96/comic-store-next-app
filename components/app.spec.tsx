beef29b480851df53168b7d6a6e94d07e5bf8adf
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2022-11-16 20:37:01 -0300

    add tests to check search functionality

--- components/app.spec.tsx
+++ components/app.spec.tsx
@@ -1,6 +1,7 @@
 import AppComponent from './app';
 import {render, screen, waitFor} from '@testing-library/react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 
 describe('AppComponent', () => {
   it('should have an input with "Buscar" placeholder', () => {
@@ -21,5 +22,30 @@ describe('AppComponent', () => {
     render(<AppComponent count={1} />);
 
     expect(document.querySelector('span')).toHaveTextContent('1 comic is available');
+  });
+
+
+  it('should search when 3 letters are written', async () => {
+    const user = userEvent.setup();
+    const onSearch = jest.fn();
+    render(<AppComponent count={1} onSearch={onSearch} />);
+
+    await user.type(document.querySelector('input'),  '123')
+
+    expect(document.querySelector('input')).toHaveValue('123');
+    expect(onSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should display options when 3 letters are written', async () => {
+    const user = userEvent.setup();
+    const onSearch = (a) => {
+
+    };
+    render(<AppComponent count={1} onSearch={onSearch} />);
+
+    await user.type(document.querySelector('input'),  '123')
+
+    expect(document.querySelector('input')).toHaveValue('123');
+    expect(document.querySelector('div.list')).toHaveTextContent('123 comic')
   });
 });
