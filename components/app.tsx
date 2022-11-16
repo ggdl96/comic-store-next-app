70560a8c2f25a686a0230a59a4789b7274f2d28d
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2022-11-16 20:40:16 -0300

    add logic to match tests

--- components/app.tsx
+++ components/app.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function AppComponent( { count } ) {
+export default function AppComponent( { count, onSearch } ) {
   const isSingle = count === 1;
   const text = `comic${ !isSingle ? 's': ''} ${ !isSingle ? 'are' : 'is'} available`;
 
-  return <div><input placeholder='Search' /><span>{count} {text}</span></div>
+  const onChange = (e) => {
+    if (e.target.value.length >= 3) {
+      onSearch();
+    }
+  }
+
+  return (
+    <div>
+      <input placeholder='Search' onChange={onChange}/>
+      <span>{count} {text}</span>
+      <div className='list'><span>123 comic</span></div>
+    </div>
+  );
 }
 
 AppComponent.propTypes = {
   count: PropTypes.number.isRequired,
+  onSearch: PropTypes.func,
 }
+
+AppComponent.defaultProps = {
+  onSearch: () => { },
+  results: [],
+};
