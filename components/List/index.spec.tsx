0516ca9fda495bf7ca5ea6a486fbe5bea41333bb
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2022-12-05 12:01:10 -0300

    Add tests for comic listing

---
+++ components/List/index.spec.tsx
@@ -0,0 +1,24 @@
+import ListComponent from './';
+import {render, screen, waitFor} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+
+describe('List', () => {
+  it('should display a list with at least one comic title', async () => {
+    render(<ListComponent items={['item 1']} />);
+
+    const foundOptions = await screen.findAllByText(/item /);
+
+    expect(screen.queryByTestId('ul-list')).toBeTruthy();
+    expect(foundOptions.length).toEqual(1);
+  });
+
+  it('should display a label when no comics', async () => {
+    render(<ListComponent items={[]} />);
+
+    const noResultsNode = await screen.findByText(/No results/);
+
+    expect(screen.queryByTestId('ul-list')).toBeFalsy();
+    expect(noResultsNode).toBeTruthy();
+  });
+});
