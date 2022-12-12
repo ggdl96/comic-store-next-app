817a0e000a91bc49c73e81f381a63238220b7572
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2022-12-11 22:03:39 -0300

    Update logic of index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head'
-
-import AppComponent from '../components/Search';
+import AppComponent from '../components/SmartAppComponent';
+import Requester from '../model/Requester';
 
 export default function Home() {
+  const requester = new Requester(['comic 1', 'comic 2']);
+
   return (
     <div>
       <Head>
         <title>Comics Store App</title>
       </Head>
-      <AppComponent />
+      <AppComponent requester={requester} />
     </div>
   )
 }
