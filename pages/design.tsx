00296693acd9fafb56ecfec370e01baa517bad86
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-02-04 14:44:33 -0300

    WIP add basic layout for home, add header

---
+++ pages/design.tsx
@@ -0,0 +1,17 @@
+import Header from "../src/components/header";
+
+function Design() {
+  return (
+    <div style={{ backgroundColor: "red" }}>
+      <Header />
+      <section></section>
+      <footer>
+        <section>
+          <h3>all</h3>
+        </section>
+      </footer>
+    </div>
+  );
+}
+
+export default Design;
