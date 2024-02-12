bc86ddebc223b8adac344e570896df7a2b0ab824
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-02-12 10:06:08 -0300

    add some links to redirect

--- src/components/comic-slider-item/index.tsx
+++ src/components/comic-slider-item/index.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 interface Props {
   color?: string;
 }
 
 const ComicSliderItem = ({ color }: Props) => {
   return (
-    <div className={`flex-none bg-${color}-500 w-36 sd:w-36 md:w-64 lg:w-96 h-full`}></div>
+    <Link href="design-detail">
+      <div className={`flex-none bg-${color}-500 w-36 sd:w-36 md:w-64 lg:w-96 h-full`}></div>
+    </Link>
   );
 };
 
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,31 +1,37 @@
+import Link from "next/link";
+
 function Header() {
   return (
-      <header className="bg-white">
-        <nav
-          className="mx-auto flex flex-row max-w-9xl items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="sm:hidden">
-            <h3>Menu</h3>
-          </div>
-          <div className="flex flex-row justify-between grow-0">
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex flex-row max-w-9xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="sm:hidden">
+          <h3>Menu</h3>
+        </div>
+        <div className="flex flex-row justify-between grow-0">
+          <Link href="/design">
             <h1>comics</h1>
-          </div>
-          <div className="flex flex-row grow space-x-2 max-sm:hidden">
-            <h2 className="text-xl">all</h2>
-            <h2 className="text-xl">best sellers</h2>
-          </div>
-          <div className="sm:hidden">
-            <h3>Search M</h3>
-          </div>
-          <div className="flex flex-row grow-0 justify-end items-baseline">
-            <h2 className="text-xl max-sm:hidden px-6">search</h2>
-          </div>
-          <div className="flex flex-row grow-0 justify-end items-baseline">
+          </Link>
+        </div>
+        <div className="flex flex-row grow space-x-2 max-sm:hidden">
+          <h2 className="text-xl">all</h2>
+          <h2 className="text-xl">best sellers</h2>
+        </div>
+        <div className="sm:hidden">
+          <h3>Search M</h3>
+        </div>
+        <div className="flex flex-row grow-0 justify-end items-baseline">
+          <h2 className="text-xl max-sm:hidden px-6">search</h2>
+        </div>
+        <div className="flex flex-row grow-0 justify-end items-baseline">
+          <Link href="/design-cart">
             <h2>cart</h2>
-          </div>
-        </nav>
-      </header>
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
 
