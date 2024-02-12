31b68a80226308da26dfdbc26649740805d5a828
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-02-12 20:59:00 -0300

    add new layout component for checkout and cart pages

---
+++ src/components/layouts/main-checkout/index.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+
+interface Props {
+  children: ReactElement;
+}
+
+function MainCheckout({ children }: Props) {
+  return (
+    <main className="p-4 sd:p-6 md:p-8 lg:p-12 xl:p-24 flex flex-row justify-center">
+      {children}
+    </main>
+  );
+}
+
+export default MainCheckout;
