7cf7c3414a6e93a351b79690605925b0ecdd09fc
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-02-12 21:00:01 -0300

    add input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,17 @@
+interface Props {
+    placeholder: string;
+}
+
+function Input({ placeholder }: Props) {
+  return (
+    <div className="w-full flex flex-col grow pb-2">
+      <input
+        placeholder={placeholder}
+        className="border-gray-300 border-2 rounded-md w-full p-2"
+      ></input>
+      <div className="h-14 bg-red-400 w-full"></div>
+    </div>
+  );
+}
+
+export default Input;
