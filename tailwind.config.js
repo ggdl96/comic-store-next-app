bd0e15a47739e918adbd736b938d5626329d7742
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-11-08 17:09:32 -0300

    apply prettier to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
@@ -17,5 +17,5 @@ module.exports = {
       pattern:
         /(bg|text|border)-(transparent|current|white|purple|midnight|metal|tahiti|silver|bermuda|green|emerald)/,
     },
-  ]
+  ],
 };
