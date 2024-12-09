b1d7b5803d9d8b32101f3afee04ca07cf6d59b95
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-12-09 13:55:45 -0300

    update cypress tests

--- cypress/e2e/pages/design-detail/design-detail.cy.ts
+++ cypress/e2e/pages/design-detail/design-detail.cy.ts
@@ -1,11 +1,10 @@
-const url = '/design-detail';
+const url = '/design-detail/abc1234';
 
 describe(`go to ${url} renders correctly`, () => {
   it("contains an add to cart button and a buy now button", () => {
     cy.visit(`http://localhost:3000${url}`);
     cy.contains('comics');
 
-    
     cy.get('.add-to-cart-btn').contains('Add to cart');
     cy.get('.buy-now-btn').contains('Buy now');
   });
