7429b1eaacb1b3cb9c76c9abfa6819c6294745f1
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2025-02-22 11:28:15 -0300

    Improve layout of main-checkout

--- src/components/layouts/main-checkout/index.tsx
+++ src/components/layouts/main-checkout/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function MainCheckout({ children }: Props) {
   return (
-    <main className="p-4 sd:p-6 md:p-8 lg:p-12 xl:p-24 flex flex-row justify-center">
+    <main className="p-4 sd:p-6 md:p-8 lg:p-12 xl:p-24 flex flex-row justify-center w-full">
       {children}
     </main>
   );
