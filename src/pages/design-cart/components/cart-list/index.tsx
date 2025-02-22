376af30d8b8722ef8b2b3e08726f432dd9af438d
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2025-02-22 11:15:41 -0300

    Add cart list component

---
+++ src/pages/design-cart/components/cart-list/index.tsx
@@ -0,0 +1,54 @@
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import Button from '../../../../components/button';
+import CartItem from '../../../../components/cart-item';
+import { removeComicItemFromCart } from '../../../../features/cart';
+import { ComicItemDetail } from '../../../../model/components/ComicItemDetail';
+import { AppDispatch } from '../../../../store';
+
+interface PropsCartList {
+  comicsInCart: ComicItemDetail[];
+  currency: string;
+  price: number;
+}
+
+function CartList({ comicsInCart, currency, price }: PropsCartList) {
+  const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <>
+      <section className="cart-item-list">
+        {comicsInCart.map(item => (
+          <CartItem
+            key={item.id}
+            item={item}
+            onDelete={() => {
+              dispatch(removeComicItemFromCart(item.id));
+            }}
+          />
+        ))}
+      </section>
+      <section>
+        <div className="flex flex-row justify-end py-4">
+          <span className="cart-total-cost">
+            total: ({currency}){price}
+          </span>
+        </div>
+      </section>
+      <section className="flex flex-row justify-center">
+        <div className="w-full sm:w-3/5 md:w-6/12 lg:w-5/12 xl:w-3/12">
+          <Button
+            className="checkout-button"
+            onClick={() => {
+              router.push('/design-checkout');
+            }}>
+            Checkout
+          </Button>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default CartList;
