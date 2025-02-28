739b015fcf395e20b3883176bd9f67b2ff7e270c
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2025-02-28 10:17:58 -0300

    Improve cart actions

--- src/components/cart-item/index.tsx
+++ src/components/cart-item/index.tsx
@@ -1,17 +1,38 @@
 import Image from 'next/image';
 import { MouseEventHandler } from 'react';
 import { ComicItemCart } from '../../model/components/ComicItemCart';
+interface ItemQuantityProps {
+  onAdd: () => void;
+  onSubtract: () => void;
+  quantity: number;
+  stock: number;
+}
 
-function ItemQuantity() {
-  return <div className="flex justify-end">- 1 +</div>;
+function ItemQuantity({ onAdd, onSubtract, quantity, stock }: ItemQuantityProps) {
+  const removeDisabled = quantity === 1;
+  const addDisabled = quantity === stock;
+
+  return (
+    <div className="flex justify-end items-center">
+      <button disabled={removeDisabled} className="cart-subtract-item p-2" onClick={onSubtract}>
+        -
+      </button>
+      <p className="cart-item-quantity p-2">{quantity}</p>
+      <button disabled={addDisabled} className="cart-add-item p-2" onClick={onAdd}>
+        +
+      </button>
+    </div>
+  );
 }
 
 interface Props {
   item: ComicItemCart;
   onDelete: MouseEventHandler<HTMLButtonElement>;
+  onAdd: () => void;
+  onSubtract: () => void;
 }
 
-function CartItem({ item, onDelete }: Props) {
+function CartItem({ item, onDelete, onAdd, onSubtract }: Props) {
   return (
     <div className="cart-item w-full h-36 grid-rows-2 sm:grid-rows-1">
       <div className="grid grid-cols-12 w-full h-24 sm:h-36">
@@ -28,7 +49,12 @@ function CartItem({ item, onDelete }: Props) {
         </div>
         <div className="hidden sm:grid sm:col-span-2 sm:grid-cols-2">
           <div></div>
-          <ItemQuantity />
+          <ItemQuantity
+            onAdd={onAdd}
+            onSubtract={onSubtract}
+            quantity={item.quantity}
+            stock={item.stock}
+          />
           <button onClick={onDelete}>Delete</button>
         </div>
         <div className="bg-gray-500 col-span-4 flex flex-row justify-end">
@@ -39,7 +65,12 @@ function CartItem({ item, onDelete }: Props) {
       </div>
       <div className="grid grid-cols-2 sm:hidden">
         <div></div>
-        <ItemQuantity />
+        <ItemQuantity
+          onAdd={onAdd}
+          onSubtract={onSubtract}
+          quantity={item.quantity}
+          stock={item.stock}
+        />
         <button className="button-delete-item" onClick={onDelete}>
           Delete
         </button>
