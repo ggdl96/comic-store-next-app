2054c1a7faf9a13c1f1a3d371fbfe34815662e80
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2025-03-09 17:09:40 -0300

    Add mocked data for brands

---
+++ __mocks__/data/brands/index.ts
@@ -0,0 +1,27 @@
+export const COMIC_BRANDS = [
+  {
+    id: 'brand-1',
+    name: 'Aurora Comics',
+    image: '/aurora-comics-logo.png',
+  },
+  {
+    id: 'brand-2',
+    name: 'Nova Spire',
+    image: '/nova-spire-logo.png',
+  },
+  {
+    id: 'brand-3',
+    name: 'Pulsar Press',
+    image: '/pulsar-press-logo.png',
+  },
+  {
+    id: 'brand-4',
+    name: 'Ravenwood Publishing',
+    image: '/ravenwood-publishing-logo.png',
+  },
+  {
+    id: 'brand-5',
+    name: 'Spectra Comics',
+    image: '/spectra-comics-logo.png',
+  },
+] as const;
